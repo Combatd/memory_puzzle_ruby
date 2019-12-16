dbcf6fbf9122812e7d37f9d50a74a4a5ac3ca343
Combatd <combatdstar@gmail.com>
2019-12-16 14:34:11 -0800

    create Board.render method in board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -7,11 +7,12 @@ class Board
 
     def populate
         # should fill the board with a set of shuffled Card pairs
-        [0,0,1,1,2,2,3,3].shuffle.each_slice(2).to_s
+        @grid = [0,0,1,1,2,2,3,3].shuffle.each_slice(2).to_s
     end
 
     def render
-
+        # should print out a representation of the Board's current state
+        @grid.each { |pos| print pos }
     end
 
     def won?
