7e6c6c24742d8b09b6938d837a34049d5cdf763b
Combatd <combatdstar@gmail.com>
2019-12-16 13:46:27 -0800

    define game.rb constructor

--- lib/game.rb
+++ lib/game.rb
@@ -2,5 +2,9 @@ require_relative "Board"
 require_relative "Card"
 
 class Game
-
+    def initialize
+        @board = board
+        @guessed_pos = guessed_pos
+        @previous_guess = previous_guess
+    end
 end
