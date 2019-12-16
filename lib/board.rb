2a6673c63cec28a5b7185ed9ea5772eeed1963e2
Combatd <combatdstar@gmail.com>
2019-12-16 00:23:06 -0800

    setup boilerplate for Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,24 @@
 require_relative "memory_puzzle"
+
+class Board
+    def initialize
+        @grid = grid
+    end
+
+    def populate
+
+    end
+
+    def render
+
+    end
+
+    def won?
+
+    end
+
+    def reveal
+
+    end
+
+end
